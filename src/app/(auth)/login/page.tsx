668ba6d38f409bdd8202764aa0bdf3c8f7d8a508
Tushar Khatter <tushar@sourcewiz.co>
2024-10-23 23:17:33 +0530

    chore: installed drawer package

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,87 +1,111 @@
-'use client'
+"use client";
 // eslint-disable
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { useRouter } from "next/navigation"
-import { Form, FormControl,  FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { useToast } from "@/hooks/use-toast"
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 
-const  SignIn =()=> {
-  const {toast}  = useToast()
+const SignIn = () => {
+  const { toast } = useToast();
   const router = useRouter();
-  const [ isSubmitting, setIsSubmitting ] = useState<boolean>(false);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   const form = useForm({
     defaultValues: {
-      identifier: '',
-      password: '',
-    }
+      identifier: "",
+      password: "",
+    },
   });
 
-  
   const onSubmit = async (data: any) => {
-    document.cookie='access_token=[value]'
-    router.replace('/dashboard');
+    document.cookie = "access_token=[value]";
+    router.replace("/dashboard");
     toast({
       title: "Scheduled: Catch up",
       description: "Friday, February 10, 2023 at 5:57 PM",
-    })
+    });
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className=" w-full max-w-md p-8 space-y-8 rounded-lg bg-white shadow-md">
-       <div className="text-center">
-          <h2 className="text-2xl font-bold text-center tracking-tight mb-6">Sign In</h2>
-       </div>
-     
+      <div className="w-full max-w-md p-8 space-y-8 rounded-lg bg-white shadow-md">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold text-center tracking-tight mb-6">
+            Sign In
+          </h2>
+        </div>
+
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField control={form.control} name="identifier" render={()=>(
-              <FormItem>
-               <FormField
-                  name="identifier"
-                  control={form.control}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email / Username</FormLabel>
-                      <FormControl>
-                        <Input placeholder="email/username" {...field} onChange={(e)=> field.onChange(e)}/>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              
-              </FormItem>
-            )}/>
-              <FormField control={form.control} name="password" render={()=>(
-              <FormItem>
-               <FormField
-                  name="password"
-                  control={form.control}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input type="password" placeholder="password" {...field} onChange={(e)=> field.onChange(e)}/>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </FormItem>
-            )}/>
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={() => (
+                <FormItem>
+                  <FormField
+                    name="identifier"
+                    control={form.control}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email / Username</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="email/username"
+                            {...field}
+                            onChange={(e) => field.onChange(e)}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={() => (
+                <FormItem>
+                  <FormField
+                    name="password"
+                    control={form.control}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="password"
+                            {...field}
+                            onChange={(e) => field.onChange(e)}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </FormItem>
+              )}
+            />
             <Button type="submit" disabled={isSubmitting} className="w-full">
-              {isSubmitting ?  ' Please wait': 'Sign In'  }
+              {isSubmitting ? " Please wait" : "Sign In"}
             </Button>
           </form>
         </Form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignIn;
